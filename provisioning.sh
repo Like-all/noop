b185402a681a66167672508ce5f1bc523b8fa8ef
like-all <like-all@yandex.com>
2017-03-29 21:49:27 +0300

    Updated provisioning script

--- provisioning.sh
+++ provisioning.sh
@@ -6,3 +6,5 @@ apt-get -y install apt-transport-https
 echo "deb https://dl.bintray.com/like-all/deb jessie-testing main" > /etc/apt/sources.list.d/bintray.list
 apt-get -qq update
 apt-get -y install noop
+rm -f /sbin/init
+ln -s /sbin/noop /sbin/init
