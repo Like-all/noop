48b85433982eeb16a0c87002b8ebf001b15b4282
like-all <like-all@yandex.com>
2017-03-29 19:10:00 +0300

    Vagrant provisioning script

---
+++ provisioning.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+wget -c -O - https://bintray.com/user/downloadSubjectPublicKey?username=bintray | apt-key add -
+apt-get -qq update
+apt-get -y install apt-transport-https
+echo "deb https://dl.bintray.com/like-all/deb jessie-testing main" > /etc/apt/sources.list.d/bintray.list
+apt-get -qq update
+apt-get -y install noop
