c65936813575f868bccb90cae878d248cab0f869
Like All <like-all@yandex.com>
2017-07-19 02:04:46 -0700

    Added spew output

--- src/noop/main.go
+++ src/noop/main.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 import "time"
+import "github.com/davecgh/go-spew/spew"
 
 func main() {
     config, _ := loadConfig()
+    spew.Dump(config)
     fmt.Println("Entry point: " + config.Noop.EntryPoint)
     for {
         fmt.Println("This is noop")
